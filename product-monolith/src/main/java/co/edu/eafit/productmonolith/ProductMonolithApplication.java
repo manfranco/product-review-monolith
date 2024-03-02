a84e4c52b2515115406495c9a6783f78cabbcc68
MarcoFranco <marco.franco@kinandcarta.com>
2024-03-02 16:57:46 -0500

    initial commit

---
+++ product-monolith/src/main/java/co/edu/eafit/productmonolith/ProductMonolithApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.productmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMonolithApplication.class, args);
+	}
+
+}
